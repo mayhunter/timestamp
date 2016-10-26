75c6a8e623bf59532dec93079373d965d9d40fef
mayhunter <mayhunter356@hotmail.com>
2016-10-26 16:21:09 +0000

    Timestamp server complete

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
-var express = require("express");
+var express = require('express');
 var app = express();
 
-app.get('/',function(req, res){
-    res.send('<h1>Hello World</h1>');
+app.get('/:timestamp', function (req, res) {
+    var inputDate = req.params.timestamp;
+    if(!isNaN(inputDate)) inputDate = parseInt(inputDate) * 1000;
+    var date = new Date(inputDate);
+    var output = {'unix': null, 'natural_date': null};
+    if(!isNaN(date.getTime())){
+        output.unix = date.getTime()/1000;
+        output.natural_date = date.toDateString();
+    }
+  res.send(JSON.stringify(output));
 });
 
-app.listen(8080, function(){
-    console.log('Listening 8080');
+app.listen(8080, function () {
+  console.log('Example app listening on port 3000!');
 });
